d0e6e8423fa39922ae67f8fa040a6e6cf4e9a645
m-kubes <michaelkubiak5@gmail.com>
2026-05-06 13:16:34 -0400

    Added moving characters

--- scripts/scenes.js
+++ scripts/scenes.js
@@ -26,8 +26,16 @@ SceneManager.add_scene(new Scene("Main Menu", () => {
 
 
 SceneManager.add_scene(new Scene("Yahu Intro", async () => {
+    const benjamin_netanyahu = character_container.get_character("Benjamin Netanyahu");
     const yahu_dialogue_box = dialogue_manager.get_character("benjamin_netanyahu");
     const yahu_intro_dialogue = dialogue_manager.get_dialogue("yahu_intro_dialogue");
+    const yahu_moving_character = benjamin_netanyahu.moving_character;
+
+    yahu_moving_character.set_position('-100%', 'calc(40vh - 50%)');
+    yahu_moving_character.set_size('30rem', '30rem');
+    yahu_moving_character.show();
+    await yahu_moving_character.move_to('calc(50vw - 50%)', 'calc(40vh - 50%)', 2000);
 
     await yahu_dialogue_box.conversation(yahu_intro_dialogue);
+    await yahu_moving_character.move_to('100vw', 'calc(40vh - 50%)', 2000);
 }));
