40a8a88143954dbf99d4b71fa05748e0848ae694
DakotaByrd <dakota.byrd1@outlook.com>
2026-05-06 13:33:42 -0400

    Fix stats not exporting and adding money count to shop ui

--- scripts/Shop.js
+++ scripts/Shop.js
@@ -1,18 +1,27 @@
-import { inventory } from "./Inventory.js";
-// not exported yet import { stats } from "./Stats.js"  
+import { inventory } from "./inventory.js";
+import { stats } from "./stats_manager.js";  
 //import shop list here
-import items from "../resources/items/shop_items.json" with { type: "json"}
-
+import items from "../resources/jsons/items/shop_items.json" with { type: "json"}
+console.log(stats)
 
 //object for managing shop interactions
 const shop = {
     potions_for_sale: items.potions,
     upgrades_for_sale: items.upgrades,
     potion_ingredients_for_sale: items.potion_ingredients,
+    updateMoney() {
+        document.getElementById("moneyText").innerHTML = "$" + stats.money._value
+    },
+    randomShopList() {
+        let item_in_stock = []
+        item_in_stock.push(this.potion_ingredients_for_sale[0])
+        return item_in_stock
+    },
     display_items() {
-        document.getElementById("itemImg1").src = "../resources/sprites/eyeball.png"
-        console.log(document)
+        document.getElementById("itemImg1").src = item_in_stock[0].sprite
+        console.log("????")
         },
+
     buy_items(from_item_list, to_item_list, selected_item) {
         for (let i = 0; i < from_item_list.length; i++) {
 
@@ -34,7 +43,11 @@ const shop = {
 
 //console.log(shop.items_for_sale)
 for (let i = 0; i < 21; i++) {
-    shop.buy_items(shop.potions_for_sale, inventory.potions, "Potion of Goop")
+    shop.buy_items(shop.potion_ingredients_for_sale, inventory.potion_ingredients, "Eye Ball")
 }
 
-shop.display_items()
+shop.updateMoney()
+
+let item_in_stock = shop.randomShopList()
+
+shop.display_items(item_in_stock);
