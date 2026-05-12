35d3b08bc9ea4238cf362353467b174eaaa910cc
m-kubes <michaelkubiak5@gmail.com>
2026-05-12 13:28:32 -0400

    Scene tools transition fade bug fix

--- scripts/scene_tools.js
+++ scripts/scene_tools.js
@@ -2,13 +2,14 @@ export default class SceneTools {
     static transition_fade(fade_duration, wait_duration, while_waiting_function = () => {}) {
         return new Promise((resolve) => {
             const fade_div = document.createElement("div");
-            fade_div.style.position = "fixed";
+            fade_div.style.position = "absolute";
             fade_div.style.top = "0";
             fade_div.style.left = "0";
             fade_div.style.width = "100%";
             fade_div.style.height = "100%";
             fade_div.style.backgroundColor = "black";
             fade_div.style.opacity = "0";
+            fade_div.style.zIndex = "200"
             fade_div.style.transition = `opacity ${fade_duration}ms`;
             document.body.appendChild(fade_div);
 
